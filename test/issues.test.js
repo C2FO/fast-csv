16b7a8e9a70f4c22bec618591900b5d4ae2ff2a0
Alex Tsibulya <alex@sagansystems.com>
2016-09-21 07:38:51 -0700

    Test issue 150
    - Add a test for issue #150 to specify the expected behavior
    - Mark it `skip` pending implementation

--- test/issues.test.js
+++ test/issues.test.js
@@ -239,4 +239,16 @@ it.describe("github issues", function (it) {
             });
         });
     });
+
+    it.skip("#150", function (it) {
+        it.should("not parse a row if a new line is ambiguous and there is more data", function () {
+            var data = "first_name,last_name,email_address\r";
+            var myParser = parser({delimiter: ","});
+            var parsedData = myParser(data, true);
+            assert.deepEqual(parsedData, {
+                "line": "first_name,last_name,email_address\r",
+                "rows": []
+            });
+        });
+    });
 });
