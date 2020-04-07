36a96f73c24d39bbb59626923699376fe81c82c3
doug-martin <doug@dougamartin.com>
2020-04-07 18:28:23 -0500

    Fix broken link in homepage

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -63,7 +63,7 @@ function Home() {
                                 'button button--outline button--secondary button--lg',
                                 styles.getStarted,
                             )}
-                            to={useBaseUrl('docs/doc1')}
+                            to={useBaseUrl('docs/introduction/getting-started')}
                         >
                             Get Started
                         </Link>
